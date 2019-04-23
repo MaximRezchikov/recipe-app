0e550b5df60bf7be4dfef58d2569704619742c23
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-23 16:57:05 +0300

    111.

---
+++ src/main/java/com/mr13/recipeapp/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.mr13.recipeapp.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+
+}
--- src/main/java/com/mr13/recipeapp/domain/Recipe.java
+++ src/main/java/com/mr13/recipeapp/domain/Recipe.java
@@ -26,6 +26,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -108,5 +111,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
