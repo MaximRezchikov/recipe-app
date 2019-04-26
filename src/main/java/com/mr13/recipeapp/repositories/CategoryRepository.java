f00e7ed6f068634c021c6ce4e827065abca3db69
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-26 19:31:34 +0300

    Optional

--- src/main/java/com/mr13/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/mr13/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.mr13.recipeapp.repositories;
 import com.mr13.recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/com/mr13/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/mr13/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.mr13.recipeapp.repositories;
 import com.mr13.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
