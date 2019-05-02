bb502face0108f94e3a5bed2407efe14bc7549d9
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-02 19:47:53 +0300

    Added Lombok

--- src/main/java/com/mr13/recipeapp/controllers/IndexController.java
+++ src/main/java/com/mr13/recipeapp/controllers/IndexController.java
@@ -2,10 +2,12 @@ package com.mr13.recipeapp.controllers;
 
 
 import com.mr13.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -17,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
--- src/main/java/com/mr13/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/mr13/recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.mr13.recipeapp.services;
 
 import com.mr13.recipeapp.domain.Recipe;
 import com.mr13.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the server");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
