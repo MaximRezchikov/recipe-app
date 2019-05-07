3ccc5bb756e6253dfd1ed49673a0b5181e569708
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-07 19:07:02 +0300

    Added testMockMVC()

--- src/test/java/com/mr13/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/mr13/recipeapp/controllers/IndexControllerTest.java
@@ -8,6 +8,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -17,6 +19,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -34,6 +39,16 @@ public class IndexControllerTest {
 
         controller = new IndexController(recipeService);
     }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
 
     @Test
     public void getIndexPage() {
