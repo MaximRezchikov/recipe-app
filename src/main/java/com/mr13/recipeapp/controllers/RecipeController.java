447bbda9bd968b7475150c3a4035507c616aece6
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-15 11:55:10 +0300

    fixed show.html added recipeform.html added in the RecipeController 2 methods

--- src/main/java/com/mr13/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/mr13/recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.mr13.recipeapp.controllers;
 
+import com.mr13.recipeapp.commands.RecipeCommand;
 import com.mr13.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +25,19 @@ public class RecipeController {
 
         return "/recipe/show";
 
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
